6ef97028a8e7a306021c96e98e162c16bcb06e9c
Diego Stewart <Diego@MacBook-Pro-de-Diego-2.local>
2019-07-12 12:08:14 +0200

    Updates

---
+++ precache-manifest.7652c2c797d2885048051d8f483c17f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c8c4820924e04163dcdad24426cf94",
+    "url": "/myWebsite/index.html"
+  },
+  {
+    "revision": "0c0e91a6c6a482f192f2",
+    "url": "/myWebsite/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/myWebsite/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0c0e91a6c6a482f192f2",
+    "url": "/myWebsite/static/js/main.26f287d7.chunk.js"
+  },
+  {
+    "revision": "11325f61e004574dc8d9",
+    "url": "/myWebsite/static/js/runtime~main.91cccd61.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
