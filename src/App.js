490236766ae09a572e95eb39eb89084504aac243
Diego Stewart <diegostewart1995@gmail.com>
2019-07-13 13:27:29 +0200

    Preparing development enviroment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './css/App.css';
+import { connect } from 'react-redux';
+import Web from './components/Web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Realizando pruebas 4
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return ( // Aqui podemos pasar props si es necesario a Web
+      <div> 
+        <Web/>
+      </div>
+    );
+  }
 }
+function mapStateToProps(state) {
+  return {
+    ...state
+  };
+}
+export default connect(mapStateToProps)(App);
 
-export default App;
